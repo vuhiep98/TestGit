4b5252d7cf5a39f7d772164a49b60493a315d0af
Vu Hiep <nkv.hiep@gmail.com>
2018-12-11 08:17:03 +0700

    Do some Flask examples

--- src/testFlask.py
+++ src/testFlask.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST'])
-def login():
-	user = request.form['nm']
-	return render_template('hello.html',name = user)
+@app.route('/')
+def index():
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug='true')
